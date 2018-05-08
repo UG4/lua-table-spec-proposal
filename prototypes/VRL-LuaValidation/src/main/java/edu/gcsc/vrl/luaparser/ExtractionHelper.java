c2272bb0afea8a49525e040930b599fd5a141ddc
Max Loerch <max@loerch.eu>
2018-05-08 12:37:11 +0200

    visitE - Funktion eingeführt, die Sachen wie 'type', 'style' usw. herausfinden soll

--- prototypes/VRL-LuaValidation/src/main/java/edu/gcsc/vrl/luaparser/ExtractionHelper.java
+++ prototypes/VRL-LuaValidation/src/main/java/edu/gcsc/vrl/luaparser/ExtractionHelper.java
@@ -1,4 +1,15 @@
 package edu.gcsc.vrl.luaparser;
 
 public class ExtractionHelper {
+    public static void visitE(Entry e){
+        if(e instanceof Value) {
+            System.out.println("Val-Name: "+ e.getName().toString() + "  Val: "+ ((Value) e).getValue().toString());
+            //System.out.println("Val-Name: "+ e.getName().toString());
+        } else if(e instanceof Group){
+            for(Entry h : ((Group) e).getEntries()){
+                System.out.println("Group-Name: " + e.getName().toString());
+                visitE(h);
+            }
+        }
+    }
 }
