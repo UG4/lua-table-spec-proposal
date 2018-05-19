28fc62897e7c0ed26bf8bef49acd98443a47aeab
Max Loerch <max@loerch.eu>
2018-05-19 15:57:49 +0200

    Nun werden dem 'style' entsprechend für erste Typen die richtigen Textfelder/ComboBoxen/usw gerendert inklusive Default-Value

--- prototypes/VRL-LuaValidation/src/main/java/edu/gcsc/vrl/luaparser/MyValCell.java
+++ prototypes/VRL-LuaValidation/src/main/java/edu/gcsc/vrl/luaparser/MyValCell.java
@@ -1,7 +1,11 @@
 package edu.gcsc.vrl.luaparser;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableCell;
+import javafx.scene.control.TextField;
 
 public class MyValCell extends TableCell<ValueData, ValProperty> {
     /*
@@ -16,29 +20,55 @@ public class MyValCell extends TableCell<ValueData, ValProperty> {
             setText(null);
             setStyle("");
         } else {
-            /*if (item.getType().equals("String")) {
-                setText(item.getType());
-                setStyle("");
-            } else if(item.getType().equals("Integer")){
-                setText("Integer");
-                CheckBox checkBox = new CheckBox();
-                setGraphic(checkBox);
-            }*/
-            switch(item.getType()){
-                case "String":
-                    setText(item.getType());
-                    setStyle("");
-                    break;
-                case "Double":
-                    setText("Double");
-                    CheckBox checkBox = new CheckBox();
-                    setGraphic(checkBox);
-                    break;
-                case "Integer":
-                    setText("Integer");
-                    CheckBox checkBox1 = new CheckBox();
-                    setGraphic(checkBox1);
-                    break;
+            if(item.getStyle().equals("default")) {
+                switch (item.getType()) {
+                    case "String":
+                        TextField stringField = new TextField();
+                        stringField.setText(item.getType());
+                        setStyle("");
+                        break;
+                    case "Double":
+                        if(item.getDefaultVal() != null){
+                            TextField doubleField = new TextField();
+                            doubleField.setText(item.getDefaultVal());
+                            setGraphic(doubleField);
+                            setStyle("");
+                        } else {
+                            TextField doubleField = new TextField();
+                            doubleField.setText("");
+                            setGraphic(doubleField);
+                            setStyle("");
+                        }
+                        break;
+                    case "Integer":
+                        if(item.getDefaultVal() != null){
+                            TextField integerField = new TextField();
+                            integerField.setText(item.getDefaultVal());
+                            setGraphic(integerField);
+                            setStyle("");
+                        } else {
+                            TextField integerField = new TextField();
+                            integerField.setText("");
+                            setGraphic(integerField);
+                            setStyle("");
+                        }
+                        break;
+                }
+            } else if(item.getStyle().equals("selection")){
+                switch(item.getType()){
+                    case "Double":
+                        if(item.getDefaultVal() != null){
+                            ObservableList<Double> vals = FXCollections.observableArrayList();
+                            for(Double d : item.getValues()){
+                                vals.add(d);
+                            }
+                            ComboBox doubleBox = new ComboBox(vals);
+                            doubleBox.getSelectionModel().select(item.getDefaultVal());
+                            setGraphic(doubleBox);
+                            setStyle("");
+                        }
+                }
+
             }
         }
 
