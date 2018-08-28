8444bba43a0da8eab81dbd080acdeb7f1172116f
Max Loerch <max@loerch.eu>
2018-08-28 11:45:41 +0200

    Neues Testfile

---
+++ prototypes/VRL-LuaValidation/src/main/resources/validationtest03.lua
@@ -0,0 +1,26 @@
+problem = {
+  valueOne = {
+    {
+        type = "Integer"
+    },
+    {
+        subParam1 = {
+        	type = "Double",
+
+        	default = 1.5,
+
+        	style    = "selection",
+
+        	range    = {
+                  values = {0.5,1.0,1.5,2.0,2.5,3.0}
+                }
+        },
+        subParam2 = {
+        	type = "String"
+        }
+    }
+  },
+  valueTwo = {
+    type = "String"
+  }
+}
