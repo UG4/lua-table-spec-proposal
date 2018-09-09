0b6b9dd048b49996a6105b0f6434faa255d352d9
Max Loerch <max@loerch.eu>
2018-09-09 12:17:05 +0200

    Zwischenspeicherung

--- prototypes/VRL-LuaValidation/src/main/java/edu/gcsc/vrl/luaparser/GeneralUtil.java
+++ prototypes/VRL-LuaValidation/src/main/java/edu/gcsc/vrl/luaparser/GeneralUtil.java
@@ -1,18 +1,20 @@
 package edu.gcsc.vrl.luaparser;
 
 public final class GeneralUtil {
-    public GeneralUtil(){ throw new AssertionError(); }
+    public GeneralUtil() {
+        throw new AssertionError();
+    }
 
-    public static void disableWithAllChildNodes(ValueData v){
+    public static void disableWithAllChildNodes(ValueData v) {
         v.setDisabled(true);
         v.setSelection(false);
-        if(v.getOptions() != null){
-            for(ValueData vd : v.getOptions()){
-                if(vd.isOption()){
+        if (v.getOptions() != null) {
+            for (ValueData vd : v.getOptions()) {
+                if (vd.isOption()) {
                     vd.setDisabled(true);
                     vd.setSelection(false);
-                    if(vd.getOptions() != null){
-                        for(ValueData ve : vd.getOptions()){
+                    if (vd.getOptions() != null) {
+                        for (ValueData ve : vd.getOptions()) {
                             disableWithAllChildNodes(ve);
                         }
                     }
@@ -21,15 +23,15 @@ public final class GeneralUtil {
         }
     }
 
-    public static void enableWithAllChildNodes(ValueData v){
+    public static void enableWithAllChildNodes(ValueData v) {
         v.setDisabled(false);
 
-        if(v.getOptions() != null){
-            for(ValueData vd : v.getOptions()){
-                if(vd.isOption()){
+        if (v.getOptions() != null) {
+            for (ValueData vd : v.getOptions()) {
+                if (vd.isOption()) {
                     vd.setDisabled(false);
-                    if(vd.getOptions() != null){
-                        for(ValueData ve : vd.getOptions()){
+                    if (vd.getOptions() != null) {
+                        for (ValueData ve : vd.getOptions()) {
                             enableWithAllChildNodes(ve);
                         }
                     }
@@ -39,12 +41,12 @@ public final class GeneralUtil {
     }
 
     public static void selectAllParentNodes(ValueData v) {
-        if (v.getParentNode() != v.getRootNode()) {
-            if (v.getParentNode() != null) {
+        if (v.getParentNode() != null) {
+            if (v.getParentNode() != v.getRootNode()) {
                 if (v.getParentNode().isOption()) {
-                    v.getParentNode().setSelection(true);
+                    v.getParentNode().setSelectedNew(true);
                     if (v.getParentNode().getParentNode() != null) {
-                        selectAllParentNodes(v.getParentNode().getParentNode());
+                        selectAllParentNodes(v.getParentNode());
                     }
                 } else {
                     selectAllParentNodes(v.getParentNode());
