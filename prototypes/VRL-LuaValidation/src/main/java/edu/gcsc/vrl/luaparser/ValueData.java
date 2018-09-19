55e26c6dc76b8d3da7491a7f63c0b6be623bc202
Max Loerch <max@loerch.eu>
2018-09-20 01:26:24 +0200

    Neues Modell zur Selektion der ausgewählten Optionen/Parameter. Funktioniert. GUI aktualisiert allerdings manchmal noch nicht automatisch!

--- prototypes/VRL-LuaValidation/src/main/java/edu/gcsc/vrl/luaparser/ValueData.java
+++ prototypes/VRL-LuaValidation/src/main/java/edu/gcsc/vrl/luaparser/ValueData.java
@@ -1,6 +1,7 @@
 package edu.gcsc.vrl.luaparser;
 
 import javafx.beans.property.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -63,110 +64,214 @@ public class ValueData {
         return this.valProp;
     }
 
-    public List<ValueData> getOptions() { return this.subParams; }
+    public List<ValueData> getOptions() {
+        return this.subParams;
+    }
 
-    public void addSubParam(ValueData a) { this.subParams.add(a); }
+    public void addSubParam(ValueData a) {
+        this.subParams.add(a);
+    }
 
-    public StringProperty getValName() { return this.valName; }
+    public StringProperty getValName() {
+        return this.valName;
+    }
 
-    public StringProperty getType() { return this.type; }
+    public StringProperty getType() {
+        return this.type;
+    }
 
-    public String getDefaultVal() { return this.defaultVal; }
+    public String getDefaultVal() {
+        return this.defaultVal;
+    }
 
-    public String getStyle() { return this.style; }
+    public String getStyle() {
+        return this.style;
+    }
 
-    public String getTooltip() { return this.tooltip; }
+    public String getTooltip() {
+        return this.tooltip;
+    }
 
-    public double getRange_min() { return this.range_min; }
+    public double getRange_min() {
+        return this.range_min;
+    }
 
-    public double getRange_max() { return this.range_max; }
+    public double getRange_max() {
+        return this.range_max;
+    }
 
-    public double[] getValues() { return this.values; }
+    public double[] getValues() {
+        return this.values;
+    }
 
-    public boolean isVisible() { return this.visibility; }
+    public boolean isVisible() {
+        return this.visibility;
+    }
 
-    public ActualDataValue getActData() { return this.actData; }
+    public ActualDataValue getActData() {
+        return this.actData;
+    }
 
-    public boolean isSelected() { return this.selected.get(); }
+    public boolean isSelected() {
+        return this.selected.get();
+    }
 
-    public boolean isDisabled() { return this.disabled.get(); }
+    public boolean isDisabled() {
+        return this.disabled.get();
+    }
 
-    public ValueData getParentNode() { return this.parentNode; }
+    public ValueData getParentNode() {
+        return this.parentNode;
+    }
 
-    public boolean isAValue() { return this.isAValue; }
+    public boolean isAValue() {
+        return this.isAValue;
+    }
 
-    public String[] getValid_dependsOn() { return this.valid_dependsOn; }
+    public String[] getValid_dependsOn() {
+        return this.valid_dependsOn;
+    }
 
-    public String[] getVis_dependsOn() { return this.vis_dependsOn; }
+    public String[] getVis_dependsOn() {
+        return this.vis_dependsOn;
+    }
 
-    public boolean isValidationValid() { return this.validationIsValid; }
+    public boolean isValidationValid() {
+        return this.validationIsValid;
+    }
 
-    public Value getValid_eval() { return this.valid_eval; }
+    public Value getValid_eval() {
+        return this.valid_eval;
+    }
 
-    public Value getVis_eval() { return this.vis_eval; }
+    public Value getVis_eval() {
+        return this.vis_eval;
+    }
 
-    public boolean isOption() { return this.optGroup; }
+    public boolean isOption() {
+        return this.optGroup;
+    }
 
-    public boolean isOptValue() { return this.isOptVal; }
+    public boolean isOptValue() {
+        return this.isOptVal;
+    }
 
-    public boolean isNotOptGroup() { return this.notOptGroup; }
+    public boolean isNotOptGroup() {
+        return this.notOptGroup;
+    }
 
-    public boolean dependsOnValidate(){ return this.dependsOnValidate; }
+    public boolean dependsOnValidate() {
+        return this.dependsOnValidate;
+    }
 
-    public boolean dependsOnVisible() { return this.dependsOnVisible; }
+    public boolean dependsOnVisible() {
+        return this.dependsOnVisible;
+    }
 
-    public SimpleBooleanProperty getSelectedProp(){ return this.selected; }
+    public SimpleBooleanProperty getSelectedProp() {
+        return this.selected;
+    }
 
-    public SimpleBooleanProperty disabledProperty() { return this.disabled; }
+    public SimpleBooleanProperty disabledProperty() {
+        return this.disabled;
+    }
 
     // SETTER - Methoden
-    public void setSubParams(List<ValueData> subParams) { this.subParams = subParams; }
+    public void setSubParams(List<ValueData> subParams) {
+        this.subParams = subParams;
+    }
 
-    public void setType(String type) { this.type = new SimpleStringProperty(type); }
+    public void setType(String type) {
+        this.type = new SimpleStringProperty(type);
+    }
 
-    public void setDefaultVal(String defaultVal) { this.defaultVal = defaultVal; }
+    public void setDefaultVal(String defaultVal) {
+        this.defaultVal = defaultVal;
+    }
 
-    public void setStyle(String style) { this.style = style; }
+    public void setStyle(String style) {
+        this.style = style;
+    }
 
-    public void setTooltip(String tooltip) { this.tooltip = tooltip; }
+    public void setTooltip(String tooltip) {
+        this.tooltip = tooltip;
+    }
 
-    public void setRange_min(double range_min) { this.range_min = range_min; }
+    public void setRange_min(double range_min) {
+        this.range_min = range_min;
+    }
 
-    public void setRange_max(double range_max) { this.range_max = range_max; }
+    public void setRange_max(double range_max) {
+        this.range_max = range_max;
+    }
 
-    public void setValues(double[] values) { this.values = values; }
+    public void setValues(double[] values) {
+        this.values = values;
+    }
 
-    public void setVisibility(boolean visibility) { this.visibility = visibility; }
+    public void setVisibility(boolean visibility) {
+        this.visibility = visibility;
+    }
 
-    public void setActData(ActualDataValue dat) { this.actData = dat; }
+    public void setActData(ActualDataValue dat) {
+        this.actData = dat;
+    }
 
-    public void setDisabled(boolean disabled) { this.disabled.set(disabled); }
+    public void setDisabled(boolean disabled) {
+        this.disabled.set(disabled);
+    }
 
-    public void setSelection(boolean selected) { this.selected.set(selected); }
+    public void setSelection(boolean selected) {
+        this.selected.set(selected);
+    }
 
-    public void setParentNode(ValueData parentNode) { this.parentNode = parentNode; }
+    public void setParentNode(ValueData parentNode) {
+        this.parentNode = parentNode;
+    }
 
-    public void setOptional(boolean isOpt){ this.optGroup = isOpt; }
+    public void setOptional(boolean isOpt) {
+        this.optGroup = isOpt;
+    }
 
-    public void isValue(boolean AValue) { this.isAValue = AValue; }
+    public void isValue(boolean AValue) {
+        this.isAValue = AValue;
+    }
 
-    public void setOptVal(boolean opt){ this.isOptVal = opt; }
+    public void setOptVal(boolean opt) {
+        this.isOptVal = opt;
+    }
 
-    public void setNotOptGroup(boolean notOpt) { this.notOptGroup = notOpt; }
+    public void setNotOptGroup(boolean notOpt) {
+        this.notOptGroup = notOpt;
+    }
 
-    public void setValid_dependsOn(String[] valid_dependsOn) { this.valid_dependsOn = valid_dependsOn; }
+    public void setValid_dependsOn(String[] valid_dependsOn) {
+        this.valid_dependsOn = valid_dependsOn;
+    }
 
-    public void setValid_eval(Value valid_eval) { this.valid_eval = valid_eval; }
+    public void setValid_eval(Value valid_eval) {
+        this.valid_eval = valid_eval;
+    }
 
-    public void setVis_dependsOn(String[] vis_dependsOn) { this.vis_dependsOn = vis_dependsOn; }
+    public void setVis_dependsOn(String[] vis_dependsOn) {
+        this.vis_dependsOn = vis_dependsOn;
+    }
 
-    public void setVis_eval(Value vis_eval) { this.vis_eval = vis_eval; }
+    public void setVis_eval(Value vis_eval) {
+        this.vis_eval = vis_eval;
+    }
 
-    public void setValidationIsValid(boolean validationIsValid) { this.validationIsValid = validationIsValid; }
+    public void setValidationIsValid(boolean validationIsValid) {
+        this.validationIsValid = validationIsValid;
+    }
 
-    public void setDependsOnValidate(boolean dependsOn){ this.dependsOnValidate = dependsOn; }
+    public void setDependsOnValidate(boolean dependsOn) {
+        this.dependsOnValidate = dependsOn;
+    }
 
-    public void setDependsOnVisible(boolean dependsOn) { this.dependsOnVisible = dependsOn; }
+    public void setDependsOnVisible(boolean dependsOn) {
+        this.dependsOnVisible = dependsOn;
+    }
 
     // Objektmethoden
 
@@ -212,19 +317,19 @@ public class ValueData {
         for (char character : charsOfPath) {
             if (!"\\".equals(String.valueOf(character))) {
                 currentNameSb.append(character);
-            } else if("\\".equals(String.valueOf(character))){
+            } else if ("\\".equals(String.valueOf(character))) {
                 String currentName = currentNameSb.toString();
 
-                if(currentName.equals(".")){
+                if (currentName.equals(".")) {
                     currentNode = currentNode;
-                } else if(currentName.equals("..")){
-                    if(this.getParentNode() != null){
+                } else if (currentName.equals("..")) {
+                    if (this.getParentNode() != null) {
                         currentNode = currentNode.getParentNode();
                     } else {
                         System.out.println(currentNode.getValName().get() + " hasn't a Parent-Node!");
                     }
                 } else {
-                    if(currentNode.hasParam(currentName)){
+                    if (currentNode.hasParam(currentName)) {
                         currentNode = currentNode.getParam(currentName);
                     }
                 }
@@ -235,17 +340,17 @@ public class ValueData {
     }
 
     public ValueData getRootNode() {
-        if(this.getParentNode() != null){
+        if (this.getParentNode() != null) {
             return loopRootNode(this);
         } else {
             return this;
         }
     }
 
-    private ValueData loopRootNode(ValueData v){
+    private ValueData loopRootNode(ValueData v) {
         ValueData rootNode;
 
-        if(v.getParentNode() != null){
+        if (v.getParentNode() != null) {
             rootNode = loopRootNode(v.getParentNode());
             return rootNode;
         } else {
@@ -253,44 +358,103 @@ public class ValueData {
         }
     }
 
-    public void setSelectedNew(boolean sel){
-        ValueData act = this;
-        if(!this.isDisabled()) {
+    public void setSelectedNew(boolean sel) {
+        if (!this.isDisabled() && (this.isOption() || this.isOptValue())) {
             if (sel) {
-                if (isOption() || isOptValue()) {
-                    setSelection(true);
-                    if (getParentNode() != null && getParentNode().getOptions() != null) {
-                        GenUtil.selectParents(this);
-                        for (ValueData v : getParentNode().getOptions()) {
-                            if ((v.isOption()||v.isOptValue()) && !v.equals(act)) {
-                                GenUtil.disableWithAllChildNodes(v);
-                            } else if(v.isAValue()){
-                                v.setDisabled(true);
+                this.setSelection(true);
+                if (this.isOption() || this.isOptValue()) {
+
+                    if (this.getParentNode() != null) {
+                        // ParentNodes selecten
+                        selectParentNodes(this);
+                        // Alle Siblings disablen, die auch optional sind
+                        for (ValueData sib : this.getParentNode().getOptions()) {
+                            if (!sib.equals(this)) {
+                                disableWithChild(sib);
+                            }
+                        }
+
+
+                    }
+                }
+            } else if (!sel) {
+                this.setSelection(false);
+                if (this.isOption() || this.isOptValue()) {
+                    if (this.getParentNode() != null) {
+                        if (this.getParentNode().getOptions() != null) {
+                            for (ValueData sib : this.getParentNode().getOptions()) {
+
+                                enableWithChild(sib);
+
                             }
                         }
                     }
                 }
-            } else if(!sel){
-                if (isOption() || isOptValue()) {
-                    setSelection(false);
-                    if (getParentNode() != null && getParentNode().getOptions() != null) {
-                        for (ValueData v : getParentNode().getOptions()) {
-                            if ((v.isOption()||v.isOptValue())) {
-                                GenUtil.enableWithAllChildNodes(v);
-                            } else if(v.isAValue()) {
-                                v.setDisabled(false);
-                            }
-                        }
-                    }
+
+            }
+        }
+    }
+
+    //TEST
+    private void enableWithChild(ValueData v) {
+        if (v.isOption() || v.isOptValue()) {
+            v.setSelection(false);
+            v.setDisabled(false);
+
+            if (v.getOptions() != null) {
+                for (ValueData x : v.getOptions()) {
+                    enableWithChild(x);
+                }
+            }
+        } else {
+            v.setDisabled(false);
+            if (v.getOptions() != null) {
+                for (ValueData x : v.getOptions()) {
+                    enableWithChild(x);
                 }
             }
         }
     }
 
-    public boolean hasOptValue(){
-        if(getOptions() != null){
-            for(ValueData v : getOptions()){
-                if(v.isOptValue()){
+    private void disableWithChild(ValueData v) {
+        if (v.isOption() || v.isOptValue()) {
+            v.setDisabled(true);
+            v.setSelection(false);
+            if (v.getOptions() != null) {
+                for (ValueData x : v.getOptions()) {
+                    disableWithChild(x);
+                }
+            }
+        } else if (v.isNotOptGroup()) {
+            v.setDisabled(true);
+            if (v.getOptions() != null) {
+                for (ValueData x : v.getOptions()) {
+                    disableWithChild(x);
+                }
+            }
+        } else if (v.isAValue()) {
+            if (v.getParentNode() != null) {
+                if (!v.getParentNode().isSelected() || v.getParentNode().isDisabled()) {
+                    v.setDisabled(true);
+                }
+            }
+        }
+    }
+
+    private void selectParentNodes(ValueData v) {
+        if (v.getParentNode() != null) {
+            if (v.getParentNode().isOption() || v.getParentNode().isOptValue()) {
+                v.getParentNode().setSelectedNew(true);
+            }
+            selectParentNodes(v.getParentNode());
+        }
+    }
+    ///TEST
+
+    public boolean hasOptValue() {
+        if (getOptions() != null) {
+            for (ValueData v : getOptions()) {
+                if (v.isOptValue()) {
                     return true;
                 }
             }
