45fdd41fed047c892f412572a09c95cfe8fb8893
Max Loerch <max@loerch.eu>
2018-09-27 00:36:07 +0200

    Jetzt auch Testfall für match() in LoadLuaTest

--- prototypes/VRL-LuaValidation/src/test/java/edu/gcsc/vrl/luaparser/LoadLuaTest.java
+++ prototypes/VRL-LuaValidation/src/test/java/edu/gcsc/vrl/luaparser/LoadLuaTest.java
@@ -80,6 +80,106 @@ class LoadLuaTest {
     }
 
     @Test
-    void match() {
+    void match() throws Exception {
+        /*
+         * Setup, dass die korrekte Ausgabe von validationtest04.lua nachbilden soll
+         * */
+        List<ValueData> testList = new ArrayList<>();
+        ValueData valueOne = new ValueData("valueOne");
+        ValueData opt1 = new ValueData("1");
+        ValueData opt2 = new ValueData("2");
+        ValueData  subParam1 = new ValueData("subParam1");
+        ValueData  subParam2 = new ValueData("subParam2");
+        ValueData  valueTwo = new ValueData("valueTwo");
+
+        ActualDataValue adv1 = new ActualDataValue();
+        adv1.setType("Integer");
+        adv1.setValue(20);
+        opt1.setActData(adv1);
+        opt1.setDefaultVal(20);
+
+        ActualDataValue adv2 = new ActualDataValue();
+        adv2.setType("String");
+        adv2.setValue("d");
+        subParam1.setActData(adv2);
+        //subParam1.setDefaultVal("lala");
+
+        ActualDataValue adv3 = new ActualDataValue();
+        adv3.setType("String");
+        adv3.setValue("a");
+        subParam2.setActData(adv3);
+        //subParam2.setDefaultVal("hshs");
+
+        ActualDataValue adv4 = new ActualDataValue();
+        adv4.setType("String");
+        adv4.setValue("b");
+        valueTwo.setActData(adv4);
+        //valueTwo.setDefaultVal("lala");
+
+        valueOne.addSubParam(opt1);
+        opt1.setParentNode(valueOne);
+        valueOne.addSubParam(opt2);
+        opt2.setParentNode(valueOne);
+
+        opt2.addSubParam(subParam1);
+        subParam1.setParentNode(opt2);
+        opt2.addSubParam(subParam2);
+        subParam2.setParentNode(opt2);
+
+        testList.add(valueOne);
+        testList.add(valueTwo);
+
+        /*
+        * Load Lua-File with Params
+        * */
+        byte[] code2Data = ByteStreams.toByteArray(Main.class.getResourceAsStream("/test.lua"));
+        String code = new String(code2Data, "UTF-8");
+
+        Group importedCode = Group.toGroup(code);
+        List<ValueData> vdLua = new ArrayList<>();
+        LoadLua.visitingLuaCode(importedCode,vdLua);
+
+        /*
+        * Load validation-spec
+        * */
+        byte[] code2d = ByteStreams.toByteArray(Main.class.getResourceAsStream("/validationtest04.lua"));
+        String valicode = new String(code2d, "UTF-8");
+        Group importedSpec = Group.toGroup(valicode);
+
+        List<ValueData> programList = new ArrayList<>();
+        VisitingValidatorSpec.visitOne(importedSpec, programList);
+
+        /*
+        * Compare parameter names
+        * */
+        List<String> allNamesExpected = GenUtil.getAllVDNames(testList);
+        List<String> allNamesActual = GenUtil.getAllVDNames(programList);
+
+        assertEquals(allNamesExpected.size(),allNamesActual.size(),"Size must be equal");
+
+        for(String s : allNamesExpected){
+            assertTrue(allNamesActual.contains(s), "Parameter: " + s + " was not found");
+        }
+
+        /*
+         * Do matching
+         * */
+        LoadLua.match(programList,vdLua);
+
+        /*
+        * Compare new parameter values
+        * */
+        List<ValueData> vdExpected = GenUtil.getAllVD(testList);
+        List<ValueData> vdActual = GenUtil.getAllVD(programList);
+
+        for(int i = 0 ; i < vdExpected.size(); i++){
+            ValueData actActual = vdActual.get(i);
+            ValueData actExpected = vdExpected.get(i);
+
+            if(actExpected.getActData() != null){
+                assertTrue(actExpected.getActData().getValue().toString().equals(actActual.getActData().getValue().toString()),"Value is not equal!");
+            }
+        }
+
     }
 }
