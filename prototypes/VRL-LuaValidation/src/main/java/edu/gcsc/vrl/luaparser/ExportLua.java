c33c7e089604811e69c2ef05451d1d4b8c33370a
Max Loerch <max@loerch.eu>
2018-09-27 10:47:05 +0200

    JavaDoc

--- prototypes/VRL-LuaValidation/src/main/java/edu/gcsc/vrl/luaparser/ExportLua.java
+++ prototypes/VRL-LuaValidation/src/main/java/edu/gcsc/vrl/luaparser/ExportLua.java
@@ -2,12 +2,22 @@ package edu.gcsc.vrl.luaparser;
 
 import java.util.List;
 
+/**
+ * This class creates a lua-conform string to write it in a lua-file.
+ * This contains the actual values and params, the user selected
+ * */
 public final class ExportLua {
 
     public ExportLua() {
         throw new AssertionError();
     }
 
+    /**
+     * Builds the export-string
+     *
+     * @param data actual data set
+     * @param filename file name of validation-file
+     * */
     public static String doExport(List<ValueData> data, String filename) {
         StringBuilder sb = new StringBuilder();
         sb.append("problem={");
@@ -64,6 +74,14 @@ public final class ExportLua {
         return str;
     }
 
+    /**
+     * Builds and adds the string for sub-parameters
+     *
+     * @param dis distance from root
+     * @param last is last iteration
+     * @param sb <code>StringBuilder</code>
+     * @param vList actual data set
+     * */
     private static void doSubParams(List<ValueData> vList, StringBuilder sb, int dis, boolean last) {
         for (int i = 0; i < vList.size(); i++) {
             if (vList.get(i).isAValue() && vList.get(i).isSelected()) {
@@ -96,6 +114,14 @@ public final class ExportLua {
         }
     }
 
+    /**
+     * Builds and adds the string for values
+     *
+     * @param dis distance from root
+     * @param last is last iteration
+     * @param sb <code>StringBuilder</code>
+     * @param vData actual data set
+     * */
     private static void doVal(ValueData vData, StringBuilder sb, int dis, boolean last) {
         if (vData.getActData() != null && vData.getActData().getValue() != null) {
             sb.append(vData.getValName().get() + "=");
@@ -103,12 +129,28 @@ public final class ExportLua {
         }
     }
 
+    /**
+     * Builds and adds the string for optional values
+     *
+     * @param dis distance from root
+     * @param last is last iteration
+     * @param sb <code>StringBuilder</code>
+     * @param vData actual data set
+     * */
     private static void doOptVal(ValueData vData, StringBuilder sb, int dis, boolean last) {
         if (vData.getActData() != null && vData.getActData().getValue() != null) {
             doStr(vData, sb);
         }
     }
 
+    /**
+     * Builds and adds the string for not-optional groups
+     *
+     * @param dis distance from root
+     * @param last is last iteration
+     * @param sb <code>StringBuilder</code>
+     * @param vList actual data set
+     * */
     public static void doNotOpt(List<ValueData> vList, StringBuilder sb, int dis, boolean last) {
         sb.append("{");
 
@@ -160,6 +202,14 @@ public final class ExportLua {
         sb.append("}");
     }
 
+    /**
+     * Builds and adds the string for optional groups
+     *
+     * @param dis distance from root
+     * @param last is last iteration
+     * @param sb <code>StringBuilder</code>
+     * @param vList actual data set
+     * */
     public static void doOption(List<ValueData> vList, StringBuilder sb, int dis, boolean last) {
         for (int i = 0; i < vList.size(); i++) {
             if (vList.get(i).isAValue() && vList.get(i).isSelected()) { // Value
@@ -199,6 +249,9 @@ public final class ExportLua {
         }
     }
 
+    /**
+     * Helping method
+     * */
     private static String indent(int n) {
         String s = "  ";
         String result = "";
@@ -210,7 +263,9 @@ public final class ExportLua {
         return result;
     }
 
-    // Diese Funktion erstellt die Ausgabestrings der Werte für den Export in eine Lua-Datei
+    /**
+     * This method formatting the current values to a string for the export.
+     * */
     private static void doStr(ValueData vd, StringBuilder sb) {
         if (vd.getActData().getType().equals("String")) {
             String temp = GenUtil.doQuoteMark(vd.getActData().getValue().toString());
@@ -253,6 +308,9 @@ public final class ExportLua {
         }
     }
 
+    /**
+     * This method formats the export-string. It adds comma
+     * */
     private static String doFormatting(StringBuilder sb, String filename) {
         StringBuilder sbNew = new StringBuilder();
         sbNew.append("--validation file: " + filename + "\n");
@@ -272,7 +330,7 @@ public final class ExportLua {
                 if (Character.toString(sb.charAt(i + 1)).equals("{")) {
                     sbNew.append(",");
                 } else if (Character.toString(sb.charAt(i + 1)).equals("}")) {
-                    // Nichts zu tun
+
                 } else {
                     sbNew.append(",\n");
                 }
@@ -280,7 +338,7 @@ public final class ExportLua {
                 if (Character.toString(sb.charAt(i + 1)).equals("{")) {
                     sbNew.append(",\n");
                 } else if (Character.toString(sb.charAt(i + 1)).equals("}")) {
-                    // Nichts zu tun
+
                 } else {
                     sbNew.append(",\n");
                 }
@@ -291,7 +349,7 @@ public final class ExportLua {
                 if (Character.toString(sb.charAt(i + 1)).equals("{")) {
                     sbNew.append(",\n");
                 } else {
-                    // Nichts zu tun
+
                 }
             }
         }
