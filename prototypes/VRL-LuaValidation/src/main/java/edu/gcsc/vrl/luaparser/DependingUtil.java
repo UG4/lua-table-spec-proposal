fa246e6e8746669df1147f7733aab13484c16169
Max Loerch <max@loerch.eu>
2018-09-27 10:39:59 +0200

    JavaDoc

--- prototypes/VRL-LuaValidation/src/main/java/edu/gcsc/vrl/luaparser/DependingUtil.java
+++ prototypes/VRL-LuaValidation/src/main/java/edu/gcsc/vrl/luaparser/DependingUtil.java
@@ -5,12 +5,23 @@ import org.luaj.vm2.LuaValue;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This class provides methods helping to validate the visibility-property
+ * */
 public final class DependingUtil {
 
     public DependingUtil() {
         throw new AssertionError();
     }
 
+    /**
+     * Returns all the parameters a parameter depends on
+     *
+     * @param objectToValidate object to check
+     * @param runtimeData the actual data set
+     * @return List<ValueData> list of params
+     *
+     * */
     public static List<ValueData> validateAValue(ValueData objectToValidate, List<ValueData> runtimeData) {
         List<ValueData> visibObjDependsOn = new ArrayList<>();
 
@@ -22,12 +33,16 @@ public final class DependingUtil {
                     visibObjDependsOn.add(act);
                 }
             }
-            // Was wird mit den Werten gemacht
         }
         return visibObjDependsOn;
     }
 
-    // Alle Parameter mit einer dependsOn-Abhängigkeit herausfinden( bzgl. visible)
+    /**
+     * Returns all parameters, which depend on other parameter
+     *
+     * @param dataToSearch data to search
+     * @return List<ValueData> depending parameters
+     * */
     public static List<ValueData> getAllDependingVisibleValues(List<ValueData> dataToSearch) {
         List<ValueData> dependingValues = new ArrayList<>();
 
@@ -45,7 +60,9 @@ public final class DependingUtil {
         return dependingValues;
     }
 
-    // Hilfsfunktion für dependsOn-Funktion
+    /**
+     * Helping method
+     * */
     private static void searchDependingVisibleValues(List<ValueData> dependingValues, ValueData actObj) {
         if (actObj.dependsOnVisible() && !dependingValues.contains(actObj)) {
             dependingValues.add(actObj);
@@ -58,39 +75,35 @@ public final class DependingUtil {
     }
 
     public static boolean validateVisible(ValueData vd, List<ValueData> runtimeData) {
-        // Als erstes wird überprüft, ob der Parameter eine eval-Funktion hat.
-        // Wenn nicht, dann ist die Validation immer true, da ja kein 'falscher'
-        // Fall eintreten kann.
+        /**
+         * First: checks, whether the object has a evaluation-function
+         * */
         if (vd.getVis_eval() != null) {
-            // Hier werden alle Parameter herausgesucht, von denen er abhängig ist
+            // Get all parameters, the parameter depends on
             List<ValueData> dependsOn = new ArrayList<>();
             if (vd.dependsOnVisible()) {
                 dependsOn = validateAValue(vd, runtimeData);
             }
             boolean valid = true;
-            // Die Parameter von denen er abhängig ist, werden validiert.
-            // Erst wenn alle diese Parameter valide sind, kann der eigentliche
-            // Parameter validiert werden.
+            // validate these parameters
             for (int i = 0; i < dependsOn.size(); i++) {
                 boolean temp = validateVisible(dependsOn.get(i), runtimeData);
                 if (temp == false) {
                     valid = false;
                 }
             }
-            // Hier wird der Parameter validiert
+            // validate the parameter to check
             if (valid) {
 
                 List<ValueData> vals = new ArrayList<>();
 
-                // Alle Werte der von denen der Parameter abhängig ist, werden herausgesucht
                 for (ValueData v : dependsOn) {
                     vals.add(getActualData(v, runtimeData));
                 }
 
                 List<Value> valsForEval = new ArrayList<>();
 
-                // Hier werden die Werte, die als Argument für die eval-Funktion dienen
-                // in einer Liste gespeichert.
+                // Saves all values as an Value-object
                 if (vd.getActData() != null && vd.getActData().getValue() != null) {
                     System.out.println("Test " + vd.getValName().get());
                     doArgList(vd, valsForEval);
@@ -101,7 +114,7 @@ public final class DependingUtil {
                 }
 
                 String resultOfEval = "";
-                // Jetzt wird versucht die eval-Funktion mit den Argumenten aufzurufen
+                // try to invoke the evalute-function with the arguments
                 try {
                     resultOfEval = vd.getVis_eval().asFunction().eval(valsForEval).getValueAsString();
                 } catch (Exception ex) {
@@ -115,8 +128,6 @@ public final class DependingUtil {
 
                 return result;
             } else {
-                // Falls einer der Parameter, von denen der eigentliche Parameter abhängig ist, nicht valide ist,
-                // kann der zu überprüfende Parameter auch nicht valide sein -> return false
                 return false;
             }
 
@@ -126,9 +137,13 @@ public final class DependingUtil {
 
     }
 
+    /**
+     * Creates the arguments as <code>Value</code>-objects
+     *
+     * @param vals <code>Value</code>-list
+     * @param vData data to add
+     * */
     private static void doArgList(List<ValueData> vData, List<Value> vals) {
-        // Hier wird die List von Values erstellt, die die Argumente für die eval-Funktion enthalten.
-        // WEITERE DATENTYPEN HINZUFÜGEN: ARRAYS UND FUNCTION
         for (ValueData v : vData) {
             if (v.getActData() != null && v.getActData().getValue() != null) {
                 Object temp = v.getActData().getValue();
@@ -156,7 +171,6 @@ public final class DependingUtil {
     }
 
     private static void doArgList(ValueData v, List<Value> vals) {
-        // Hier kann ein einzelnes Argument der Argumentenliste hinzugefügt werden
         Object temp = v.getActData().getValue();
         String name = v.getValName().get();
 
@@ -180,12 +194,11 @@ public final class DependingUtil {
 
     }
 
+    /**
+     * Helping method
+     * */
     private static ValueData getActualData(ValueData vd, List<ValueData> runtimeData) {
-        // Hier werden die Werte der Parameter herausgesucht, die übergeben werden sollen
         if (!vd.dependsOnVisible()) {
-            // Wenn der Parameter von keinem Anderen abhängig ist, kann direkt das ValueData-Objekt
-            // mit dem Value zurückgegeben werden.(dafür muss dieses ValueData-Objekt natürlich auch einen
-            // Value besitzen)
             if (vd.getActData() != null && vd.getActData().getValue() != null) {
                 return vd;
             } else {
@@ -193,14 +206,8 @@ public final class DependingUtil {
                 return null;
             }
         } else {
-            // Falls der Parameter abbhängig ist, müssen die Parameter von denen der zu überprüfende
-            // Parameter abhängig ist, natürlich auch erst überprüft werden.
             List<ValueData> dependsOn = validateAValue(vd, runtimeData);
             boolean valid = true;
-
-            //Falls einer der Parameter nicht valide ist, wird null zurückgegeben.
-            // EVENTUELL ÄNDERN
-            // Wenn alle Parameter valide sind, wird der zu überprüfende Parameter zurückgegeben.
 
             for (int i = 0; i < dependsOn.size(); i++) {
                 boolean temp = validateVisible(dependsOn.get(i), runtimeData);
@@ -211,7 +218,6 @@ public final class DependingUtil {
             if (valid) {
                 return vd;
             }
-
         }
         return null;
     }
